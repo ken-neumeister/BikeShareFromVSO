4b8da94596802490131c623f3604edf016854a0e
Ken Neumeister <ken.neumeister@verizon.net>
2015-06-22 09:23:14 -0400

    Account for long trips
    Related Work Items: #15

--- BikeShareMapReduce/BikeShareMapReduce/BikeShareMapReduce.py
+++ BikeShareMapReduce/BikeShareMapReduce/BikeShareMapReduce.py
@@ -31,7 +31,30 @@ def read_subscribers(subscriber_file, subscriber_dict) :
                 continue
             subscriber_dict[row[col_names['Subscriber.type']]] = row[col_names['Subscriber.id']]
 
-def parse_traffic_file(input_file, traffic_out_file, station_out_file, quant, station_dict, subscriber_dict) :
+def distr_bike(start_time, end_time, quant, start_id, end_id, subscription_id, trip_cat, traffic_bikes) :
+    computed_duration = end_time - start_time
+    for t in range(math.floor(start_time/quant)*quant, math.floor(end_time/quant)*quant, quant) :
+        t_begin = max(t, start_time)
+        t_end = min(t+quant-1, end_time)
+        if computed_duration == 0 :
+            t_factor = 1
+        else :
+            t_factor = (t_end - t_begin) / computed_duration
+        bike_key = "{:0},{},{},{},{}".format(t,start_id,end_id,subscription_id,trip_cat)
+        if bike_key in traffic_bikes :
+            traffic_bikes[bike_key] += t_factor
+        else :
+            traffic_bikes[bike_key] = t_factor
+
+def record_station(record_time, station_id, subscriber_id, bike_count, station_dict):
+    # start_time/quant)*quant, start_id, subscriber_id, -1, station_bikes
+    station_key = '{:0},{},{}'.format(record_time, station_id, subscriber_id)
+    if station_key in station_dict :
+        station_dict[station_key] += bike_count
+    else :
+        station_dict[station_key] = bike_count
+
+def parse_traffic_file(input_file, traffic_out_file, station_out_file, quant, max_dur, station_dict, subscriber_dict) :
     with open(input_file, 'r', newline = '') as f :
         csv_reader = csv.reader(f, delimiter=',')
         header_row = False
@@ -59,37 +82,23 @@ def parse_traffic_file(input_file, traffic_out_file, station_out_file, quant, st
             end_date = row[col_names['End date']]
             end_time = calendar.timegm(time.strptime(end_date, "%m/%d/%Y %H:%M"))
             # revise to include subscriber
-            station_key = '{:0},{}'.format(math.floor(start_time/quant)*quant, start_id)
-            if station_key in station_bikes :
-                station_bikes[station_key] -= 1
-            else :
-                station_bikes[station_key] = -1
+            record_station(math.floor(start_time/quant)*quant, start_id, subscription_id, -1, station_bikes)
+            record_station(math.floor(end_time/quant)*quant, end_id, subscription_id, 1, station_bikes)
 
-            station_key = '{:0},{}'.format(math.floor(end_time/quant)*quant, end_id)
-            if station_key in station_bikes :
-                station_bikes[station_key] += 1
-            else :
-                station_bikes[station_key] = 1
-
-            duration = row[col_names['Total duration (ms)']]
-            if duration.isnumeric == False or int(duration) > 5400000 :
-                # Several extremely long trips observed probably involving stop-overs
-                # focus on this study is for station-to-station trips
-                continue
+            #if duration.isnumeric == False or int(duration) > 5400000 :
+            #duration = row[col_names['Total duration (ms)']]
+            #    # Several extremely long trips observed probably involving stop-overs
+            #    # focus on this study is for station-to-station trips
+            #    continue
 
             computed_duration = end_time - start_time
-            for t in range(math.floor(start_time/quant)*quant, math.floor(end_time/quant)*quant, quant) :
-                t_begin = max(t, start_time)
-                t_end = min(t+quant-1, end_time)
-                if computed_duration == 0 :
-                    t_factor = 1
-                else :
-                    t_factor = (t_end - t_begin) / computed_duration
-                bike_key = "{:0},{},{},{}".format(t,start_id,end_id,subscription_id)
-                if bike_key in traffic_bikes :
-                    traffic_bikes[bike_key] += t_factor
-                else :
-                    traffic_bikes[bike_key] = t_factor
+            if computed_duration <= max_dur :
+                distr_bike(start_time, end_time, quant, start_id, end_id, subscription_id, "nonstop", traffic_bikes)
+            else :
+                distr_bike(start_time, start_time+(max_dur/2), quant, start_id, end_id, subscription_id, "begin", traffic_bikes)
+                distr_bike(end_time-(max_dur/2), end_time, quant, start_id, end_id, subscription_id, "end", traffic_bikes)
+                record_station(math.floor((start_time+2700)/quant)*quant, -1, subscription_id, 1, station_bikes)
+                record_station(math.floor((end_time-2700)/quant)*quant, -1, subscription_id, -1, station_bikes)
 
             linecount += 1
             #if linecount > 1000 :
@@ -97,22 +106,21 @@ def parse_traffic_file(input_file, traffic_out_file, station_out_file, quant, st
 
     with open(station_out_file, 'w', newline = '') as f :
         csv_writer = csv.writer(f, delimiter=',')
-        row = ['Starttime', 'StartID', 'Bikes']
+        row = ['Starttime', 'StationID', 'SubscriberID', 'Bikes']
         csv_writer.writerow(row)
         for station_key in station_bikes :
-            [st_time, st_id] = station_key.split(',')
-            row = [st_time, st_id, station_bikes[station_key]]
-            csv_writer.writerow(row) 
+            [st_time, st_id, sb_id] = station_key.split(',')
+            row = [st_time, st_id, sb_id, station_bikes[station_key]]
+            csv_writer.writerow(row)
 
     with open(traffic_out_file, 'w', newline = '') as f :
         csv_writer = csv.writer(f, delimiter=',')
-        row = ['Starttime', 'StartID', 'EndID', 'SubscriberID', 'Bikes']
+        row = ['Starttime', 'StartID', 'EndID', 'SubscriberID', 'TripCategory', 'Bikes']
         csv_writer.writerow(row)
         for bike_key in traffic_bikes :
-            [st_time, st_id, end_id, sub_id] = bike_key.split(',')
-            row = [st_time, st_id, end_id, sub_id, traffic_bikes[bike_key]]
-            csv_writer.writerow(row) 
-
+            [st_time, st_id, end_id, sub_id, trip] = bike_key.split(',')
+            row = [st_time, st_id, end_id, sub_id, trip, traffic_bikes[bike_key]]
+            csv_writer.writerow(row)
 
 if __name__ == "__main__" :
     station_dict = {}
@@ -132,9 +140,10 @@ if __name__ == "__main__" :
         print ('Failed to find first entry in subscribers')
     
     traffic_file = data_path + r"\2015-Q1-Trips-History-Data.csv"
-    parsed_traffic_file = data_path + r"\2015-Q1-Parsed-Trips.csv"
-    parsed_station_file = data_path + r"\2015-Q1-Parsed_Stations.csv"
+    parsed_traffic_file = data_path + r"\2015-Q1-Parsed-Trips_v01.csv"
+    parsed_station_file = data_path + r"\2015-Q1-Parsed_Stations_v01.csv"
     quantization = 600
-    parse_traffic_file(traffic_file, parsed_traffic_file, parsed_station_file, quantization, station_dict, subscriber_dict)
+    max_dur = 3*3600 # more than 3 hours assume trip had a stop
+    parse_traffic_file(traffic_file, parsed_traffic_file, parsed_station_file, quantization, max_dur, station_dict, subscriber_dict)
 
 
